4927d8817eb98d7399b3ef5e2048617084900d8d
Orchid Sohag <shohagrana102@gmail.com>
2018-10-04 14:24:34 +0600

    Master and Home added

---
+++ public/js/theme.js
@@ -0,0 +1,518 @@
+// -----------------------------
+
+//   js index
+/* =================== */
+/*  
+    
+
+
+*/
+// -----------------------------
+
+(function($) {
+    "use strict";
+
+
+    /*---------------------
+    preloader
+    --------------------- */
+
+    $(window).on('load', function() {
+        $('#preloader').fadeOut('slow', function() { $(this).remove(); });
+    });
+
+    /*----------------------------
+     jQuery MeanMenu
+    ------------------------------ */
+    $('nav#dropdown').meanmenu();
+
+    /*-----------------
+    meanmenu 
+    -----------------*/
+    $('nav#mobile_menu_active').meanmenu({
+        meanScreenWidth: "991",
+        meanMenuContainer: '.mobile-menu-area .container',
+    });
+
+    /*------------------------------
+        datepicker
+    ------------------------------ */
+    $("#datepicker").datepicker();
+
+    /*------------------------------
+        datepicker2
+    ------------------------------ */
+    $("#datepicker2").datepicker();
+
+    /*------------------------------
+        datepicker3
+    ------------------------------ */
+    $("#datepicker3").datepicker();
+
+    /*------------------------------
+        datepicker4
+    ------------------------------ */
+    $("#datepicker4").datepicker();
+
+    /*------------------------------
+        datepicker5
+    ------------------------------ */
+    $("#datepicker5").datepicker();
+
+    /*------------------------------
+        datepicker6
+    ------------------------------ */
+    $("#datepicker6").datepicker();
+
+    /*------------------------------
+        datepicker7
+    ------------------------------ */
+    $("#datepicker7").datepicker();
+
+    /*------------------------------
+        datepicker8
+    ------------------------------ */
+    $("#datepicker8").datepicker();
+
+    /*------------------------------
+        datepicker9
+    ------------------------------ */
+    $("#datepicker9").datepicker();
+
+    /*------------------------------
+         counter
+    ------------------------------ */
+    $('.counter-up').counterUp();
+
+    /*---------------------
+    countdown
+    --------------------- */
+    $('[data-countdown]').each(function() {
+        var $this = $(this),
+            finalDate = $(this).data('countdown');
+        $this.countdown(finalDate, function(event) {
+            $this.html(event.strftime('<span class="cdown days"><span class="time-count">%-D</span> <p>Days</p></span> <span class="cdown hour"><span class="time-count">%-H</span> <p>Hour</p></span> <span class="cdown minutes"><span class="time-count">%M</span> <p>Min</p></span> <span class="cdown second"> <span><span class="time-count">%S</span> <p>Sec</p></span>'));
+        });
+    });
+
+    /*-----------------------
+    cart-plus-minus-button 
+    -------------------------*/
+    $(".cart-plus-minus").append('<div class="dec qtybutton">-</div><div class="inc qtybutton">+</div>');
+    $(".qtybutton").on("click", function() {
+        var $button = $(this);
+        var oldValue = $button.parent().find("input").val();
+        if ($button.text() == "+") {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        $button.parent().find("input").val(newVal);
+    });
+
+    /*---------------------
+    fancybox
+    --------------------- */
+    $('[data-fancybox]').fancybox({
+        image: {
+            protect: true
+        }
+    });
+
+    /*---------------------
+    about-video-popup
+    --------------------- */
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 300,
+        preloader: false,
+        fixedContentPos: false
+    });
+
+    /*---------------------
+    prr-carousel
+    --------------------- */
+    function prr_carousel() {
+        var owl = $(".prr-carousel");
+        owl.owlCarousel({
+            loop: true,
+            margin: 20,
+            responsiveClass: true,
+            navigation: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            nav: false,
+            items: 1,
+            smartSpeed: 2000,
+            dots: true,
+            autoplay: false,
+            autoplayTimeout: 2000,
+            center: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1
+                },
+                760: {
+                    items: 1
+                }
+            }
+        });
+    }
+    prr_carousel();
+
+    /*---------------------
+    introduce-carousel
+    --------------------- */
+    function introduce_carousel() {
+        var owl = $(".introduce-carousel");
+        owl.owlCarousel({
+            loop: true,
+            margin: 20,
+            responsiveClass: true,
+            navigation: true,
+            navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+            nav: true,
+            items: 1,
+            smartSpeed: 2000,
+            dots: true,
+            autoplay: false,
+            autoplayTimeout: 2000,
+            center: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1
+                },
+                760: {
+                    items: 1
+                }
+            }
+        });
+    }
+    introduce_carousel();
+
+    /*---------------------
+    event-carousel
+    --------------------- */
+    function event_carousel() {
+        var owl = $(".event-carousel");
+        owl.owlCarousel({
+            loop: true,
+            margin: 20,
+            responsiveClass: true,
+            navigation: true,
+            navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+            nav: true,
+            items: 1,
+            smartSpeed: 2000,
+            dots: true,
+            autoplay: false,
+            autoplayTimeout: 2000,
+            center: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1
+                },
+                760: {
+                    items: 1
+                }
+            }
+        });
+    }
+    event_carousel();
+
+    /*---------------------
+    testimonial-carousel
+    --------------------- */
+    function testimonial_carousel() {
+        var owl = $(".testimonial-carousel");
+        owl.owlCarousel({
+            loop: true,
+            margin: 20,
+            responsiveClass: true,
+            navigation: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            nav: false,
+            items: 1,
+            smartSpeed: 2000,
+            dots: false,
+            autoplay: true,
+            autoplayTimeout: 2000,
+            center: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1
+                },
+                760: {
+                    items: 1
+                }
+            }
+        });
+    }
+    testimonial_carousel();
+
+    /*---------------------
+    hero-2-carousel
+    --------------------- */
+    function hero2_carousel() {
+        var owl = $(".hero-2-carousel");
+        owl.owlCarousel({
+            loop: true,
+            margin: 20,
+            responsiveClass: true,
+            navigation: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            nav: true,
+            items: 1,
+            smartSpeed: 2000,
+            dots: false,
+            autoplay: true,
+            autoplayTimeout: 2000,
+            center: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1
+                },
+                760: {
+                    items: 1
+                }
+            }
+        });
+    }
+    hero2_carousel();
+
+    /*---------------------
+    room-suits-carousel
+    --------------------- */
+    function room_suits_carousel() {
+        var owl = $(".room-suits-carousel");
+        owl.owlCarousel({
+            loop: true,
+            margin: 20,
+            responsiveClass: true,
+            navigation: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            nav: true,
+            items: 3,
+            smartSpeed: 2000,
+            dots: false,
+            autoplay: false,
+            autoplayTimeout: 2000,
+            center: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1
+                },
+                760: {
+                    items: 2
+                }
+            }
+        });
+    }
+    room_suits_carousel();
+
+    /*---------------------
+    offers-carousel
+    --------------------- */
+    function offers_carousel() {
+        var owl = $(".offers-carousel");
+        owl.owlCarousel({
+            loop: true,
+            margin: 20,
+            responsiveClass: true,
+            navigation: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            nav: false,
+            items: 1,
+            smartSpeed: 2000,
+            dots: true,
+            autoplay: false,
+            autoplayTimeout: 2000,
+            center: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1
+                },
+                760: {
+                    items: 1
+                }
+            }
+        });
+    }
+    offers_carousel();
+
+    /*---------------------
+    h3-hero-carousel
+    --------------------- */
+    function h3_hero_carousel() {
+        var owl = $(".h3-hero-carousel");
+        owl.owlCarousel({
+            loop: true,
+            margin: 20,
+            responsiveClass: true,
+            navigation: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            nav: true,
+            items: 1,
+            smartSpeed: 2000,
+            dots: true,
+            autoplay: false,
+            autoplayTimeout: 2000,
+            center: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1
+                },
+                760: {
+                    items: 1
+                }
+            }
+        });
+    }
+    h3_hero_carousel();
+
+    /*---------------------
+    h4-hero-carousel
+    --------------------- */
+    function h4_hero_carousel() {
+        var owl = $(".h4-hero-carousel");
+        owl.owlCarousel({
+            loop: true,
+            margin: 20,
+            responsiveClass: true,
+            navigation: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            nav: true,
+            items: 1,
+            smartSpeed: 2000,
+            dots: true,
+            autoplay: false,
+            autoplayTimeout: 2000,
+            center: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 1
+                },
+                760: {
+                    items: 1
+                }
+            }
+        });
+    }
+    h4_hero_carousel();
+
+    /*---------------------
+    gd-tab-carousel
+    --------------------- */
+    function gd_tab_carousel() {
+        var owl = $(".gd-tab-carousel");
+        owl.owlCarousel({
+            loop: true,
+            margin: 20,
+            responsiveClass: true,
+            navigation: true,
+            navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+            nav: true,
+            items: 2,
+            smartSpeed: 2000,
+            dots: true,
+            autoplay: false,
+            autoplayTimeout: 2000,
+            center: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                760: {
+                    items: 2
+                }
+            }
+        });
+    }
+    gd_tab_carousel();
+
+
+    /*---------------------
+    // Ajax Contact Form
+    --------------------- */
+
+    $('.cf-msg').hide();
+    $('form#cf button#submit').on('click', function() {
+        var fname = $('#fname').val();
+        var subject = $('#subject').val();
+        var email = $('#email').val();
+        var msg = $('#msg').val();
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+        if (!regex.test(email)) {
+            alert('Please enter valid email');
+            return false;
+        }
+
+        fname = $.trim(fname);
+        subject = $.trim(subject);
+        email = $.trim(email);
+        msg = $.trim(msg);
+
+        if (fname != '' && email != '' && msg != '') {
+            var values = "fname=" + fname + "&subject=" + subject + "&email=" + email + " &msg=" + msg;
+            $.ajax({
+                type: "POST",
+                url: "mail.php",
+                data: values,
+                success: function() {
+                    $('#fname').val('');
+                    $('#subject').val('');
+                    $('#email').val('');
+                    $('#msg').val('');
+
+                    $('.cf-msg').fadeIn().html('<div class="alert alert-success"><strong>Success!</strong> Email has been sent successfully.</div>');
+                    setTimeout(function() {
+                        $('.cf-msg').fadeOut('slow');
+                    }, 4000);
+                }
+            });
+        } else {
+            $('.cf-msg').fadeIn().html('<div class="alert alert-danger"><strong>Warning!</strong> Please fillup the informations correctly.</div>')
+        }
+        return false;
+    });
+
+
+
+}(jQuery));
---
+++ resources/views/Master.blade.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!--- Basic Page Needs  -->
+    <meta charset="utf-8">
+    <title>Home One || Grand</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Mobile Specific Meta  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Google Fonts -->
+    
+
+<!-- End: injected by Adguard -->
+<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,500,600,700" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/icofont.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/meanmenu.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    @yield("css")
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/png" href="img/icon/favicon.ico">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+</head>
+
+<body>
+    @yield("preloader")
+    <!-- header-start -->
+    <header class="header-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="h3-logo">
+                        <a href="index-3.html"><img src="img/home1/logo.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-md-10 hidden-sm hidden-xs">
+                    <div class="menu-area-3 main-menu">
+                        <nav>
+                            <ul class="menu">
+                                <li class="active"><a href="index.html">Home</a>
+                                    <ul class="dropdown">
+                                        <li><a href="index.html">Home One</a>
+                                        </li>
+                                        <li><a href="index-2.html">Home Two</a></li>
+                                        <li><a href="index-3.html">Home Three</a></li>
+                                        <li><a href="index-4.html">Home Four</a>
+                                            <ul class="tlevel-menu">
+                                                <li><a href="#">Demo</a></li>
+                                                <li><a href="#">Demo</a></li>
+                                                <li><a href="#">Demo</a></li>
+                                                <li><a href="#">Demo</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a href="blog-standard.html">Blog</a>
+                                    <ul class="dropdown">
+                                        <li><a href="blog-standard.html">Blog Standard</a></li>
+                                        <li><a href="blog-standard-2.html">Blog Standard 2</a></li>
+                                        <li><a href="blog-single.html">Blog Single</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="gallery-standard.html">Gallery</a>
+                                    <ul class="dropdown">
+                                        <li><a href="gallery-standard.html">Gallery Standard</a></li>
+                                        <li><a href="gallery-masonary.html">Gallery Masonary</a></li>
+                                        <li><a href="gallery-full-width.html">Gallery Full Width</a></li>
+                                        <li><a href="gallery-details.html">Gallery Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="rooms.html">Rooms</a>
+                                    <ul class="dropdown">
+                                        <li><a href="rooms.html">Rooms</a></li>
+                                        <li><a href="rooms-2.html">Rooms 2</a></li>
+                                        <li><a href="rooms-details.html">Rooms Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="service.html">Service</a>
+                                    <ul class="dropdown">
+                                        <li><a href="service.html">Service</a></li>
+                                        <li><a href="service-details.html">Service Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="staff.html">Staff</a>
+                                    <ul class="dropdown from-right">
+                                        <li><a href="staff.html">Staff</a>
+                                            <li><a href="staff-details.html">Staff Details</a></li>
+                                    </ul>
+                                    </li>
+                                    <li><a href="#">Pages</a>
+                                        <ul class="dropdown from-right">
+                                            <li><a href="about.html">About</a></li>
+                                            <li><a href="contact.html">Contact</a></li>
+                                            <li><a href="contact-2.html">Contact 2</a></li>
+                                            <li><a href="404.html">404</a></li>
+                                        </ul>
+                                    </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
+    <!-- mobile-menu-start -->
+    <div class="mobile-menu-area visible-xs visible-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mobile_menu">
+                        <nav id="mobile_menu_active">
+                            <ul class="menu">
+                                <li><a href="index.html">Home</a>
+                                    <ul>
+                                        <li><a href="index.html">Home One</a>
+                                        </li>
+                                        <li><a href="index-2.html">Home Two</a></li>
+                                        <li><a href="index-3.html">Home Three</a></li>
+                                        <li><a href="index-4.html">Home Four</a>
+                                            <ul>
+                                                <li><a href="#">Demo</a></li>
+                                                <li><a href="#">Demo</a></li>
+                                                <li><a href="#">Demo</a></li>
+                                                <li><a href="#">Demo</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a href="blog-standard.html">Blog</a>
+                                    <ul>
+                                        <li><a href="blog-standard.html">Blog Standard</a></li>
+                                        <li><a href="blog-standard-2.html">Blog Standard 2</a></li>
+                                        <li><a href="blog-single.html">Blog Single</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="gallery-standard.html">Gallery</a>
+                                    <ul>
+                                        <li><a href="gallery-standard.html">Gallery Standard</a></li>
+                                        <li><a href="gallery-masonary.html">Gallery Masonary</a></li>
+                                        <li><a href="gallery-full-width.html">Gallery Full Width</a></li>
+                                        <li><a href="gallery-details.html">Gallery Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="rooms.html">Rooms</a>
+                                    <ul>
+                                        <li><a href="rooms.html">Rooms</a></li>
+                                        <li><a href="rooms-2.html">Rooms 2</a></li>
+                                        <li><a href="rooms-details.html">Rooms Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="service.html">Service</a>
+                                    <ul>
+                                        <li><a href="service.html">Service</a></li>
+                                        <li><a href="service-details.html">Service Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="staff.html">Staff</a>
+                                    <ul>
+                                        <li><a href="staff.html">Staff</a></li>
+                                        <li><a href="staff-details.html">Staff Details</a></li>
+                                    </ul>
+                                </li>
+                                <li class="active"><a href="#">Pages</a>
+                                    <ul>
+                                        <li><a href="about.html">About</a></li>
+                                        <li><a href="choose-date.html">Choose Date</a></li>
+                                        <li><a href="choose-room.html">Choose Room</a></li>
+                                        <li><a href="contact.html">Contact</a></li>
+                                        <li><a href="contact-2.html">Contact 2</a></li>
+                                        <li><a href="make-payment.html">Make Payment</a></li>
+                                        <li><a href="make-payment-confirmation.html">Make Payment Confrimation</a></li>
+                                        <li><a href="404.html">404</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- mobile-menu-end -->
+    @yield("content")
+
+
+    <!-- Scripts -->
+    <script src="js/jquery-3.2.0.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/countdown.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.meanmenu.js"></script>
+    <script src="js/jquery.scrollUp.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/theme.js"></script>
+    @yield("script")
+</body>
+
+</html>
