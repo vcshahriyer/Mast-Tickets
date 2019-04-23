1aa6bfdfcfde8d9b50e68680533595c8873882d3
vcshahriyer <shahriyer99@gmail.com>
2019-04-23 16:21:23 +0600

    Database update

--- public/sql/Mast_ticket-april-23-2019.sql
+++ public/sql/Mast_ticket-april-23-2019.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2018 at 02:56 PM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
+-- Generation Time: Apr 23, 2019 at 04:19 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -48,11 +46,11 @@ CREATE TABLE `bookings` (
 --
 
 INSERT INTO `bookings` (`id`, `bus_id`, `dept_date`, `my_seats`, `booked_seats`, `pay_status`, `cs_name`, `cs_mobile`, `cs_email`, `cs_pass`, `created_at`, `updated_at`) VALUES
-(1, 1, '2018-12-07', 'A_1,A_2,B_1,B_2', '3', 'false', 'Raghib Shahriyer', '01638414864', 'vcshahriyer@gmail.com', 'admin', NULL, NULL),
-(2, 2, '2018-11-07', 'A1,A2', '2', 'false', 'Sohag Rana', '01638414865', 'sohag@gmail.com', 'admin', NULL, NULL),
-(3, 1, '2018-11-06', 'A3,B3', '2', 'false', 'Sohag Rana', '01638414865', 'sohag@gmail.com', 'admin', NULL, NULL),
-(4, 2, '2018-11-06', 'A3', '1', 'false', 'Raghib Shahriyer', '01638414864', 'vcshahriyer@gmail.com', 'admin', NULL, NULL),
-(5, 2, '2018-11-06', 'B4', '1', 'false', 'Raghib Shahriyer', '01638414864', 'vcshahriyer@gmail.com', 'admin', NULL, NULL);
+(1, 1, '2018-12-07', 'A_1,A_2,B_1,B_2', '4', 'false', 'Raghib Shahriyer', '01638414864', 'vcshahriyer@gmail.com', 'admin', NULL, NULL),
+(2, 2, '2018-12-07', 'A_1,A_2', '2', 'false', 'Sohag Rana', '01638414865', 'sohag@gmail.com', 'admin', NULL, NULL),
+(3, 1, '2018-11-06', 'A_3,B_3', '2', 'false', 'Sohag Rana', '01638414865', 'sohag@gmail.com', 'admin', NULL, NULL),
+(4, 2, '2018-11-06', 'A_3', '1', 'false', 'Raghib Shahriyer', '01638414864', 'vcshahriyer@gmail.com', 'admin', NULL, NULL),
+(5, 2, '2018-11-06', 'B_4', '1', 'false', 'Raghib Shahriyer', '01638414864', 'vcshahriyer@gmail.com', 'admin', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -71,6 +69,7 @@ CREATE TABLE `buses` (
   `arr_time` time NOT NULL,
   `seats` int(11) NOT NULL,
   `fare` int(11) NOT NULL,
+  `available` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -79,9 +78,11 @@ CREATE TABLE `buses` (
 -- Dumping data for table `buses`
 --
 
-INSERT INTO `buses` (`id`, `company_id`, `bus_model`, `bus_type`, `route_from`, `route_to`, `dept_time`, `arr_time`, `seats`, `fare`, `created_at`, `updated_at`) VALUES
-(1, 1, 'Hino, AK1J Super Plus', 'Non AC', 'Dhaka', 'Chittagong', '17:00:00', '12:00:00', 36, 480, NULL, NULL),
-(2, 2, '21, E CLASS', 'Non AC', 'Dhaka', 'Chittagong', '05:30:00', '11:30:00', 36, 480, NULL, NULL);
+INSERT INTO `buses` (`id`, `company_id`, `bus_model`, `bus_type`, `route_from`, `route_to`, `dept_time`, `arr_time`, `seats`, `fare`, `available`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Hino, AK1J Super Plus', 'Non AC', 'Dhaka', 'Chittagong', '17:00:00', '12:00:00', 36, 480, 'true', NULL, NULL),
+(2, 2, '21, E CLASS', 'Non AC', 'Dhaka', 'Chittagong', '05:30:00', '11:30:00', 36, 480, 'true', NULL, NULL),
+(4, 8, '6, Hyundai Universe', 'AC', 'Dhaka', 'Sherpur', '05:00:00', '12:30:00', 36, 500, 'true', '2018-12-06 11:04:07', '2018-12-06 11:04:07'),
+(5, 9, 'Hundai, AK1 Super Plus', 'AC', 'Dhaka', 'Khulna', '10:10:00', '16:10:00', 35, 600, 'true', '2019-04-19 05:10:48', '2019-04-19 05:10:48');
 
 -- --------------------------------------------------------
 
@@ -103,12 +104,10 @@ CREATE TABLE `companies` (
 --
 
 INSERT INTO `companies` (`id`, `name`, `owner`, `address`, `created_at`, `updated_at`) VALUES
-(1, 'Hanif Enterprise', 'Mr. Sirajul Islam Hanif', '', NULL, NULL),
-(2, 'Shyamoli NR Travels', 'Ganesh Chandra Ghosh', '', NULL, NULL),
-(3, 'Orchid', 'Rana', '', '2018-12-03 14:09:42', '2018-12-03 14:09:42'),
-(4, 'Orchid', 'Rana', '', '2018-12-03 14:15:48', '2018-12-03 14:15:48'),
-(5, 'mast', 'Raghib shahriyer', '', '2018-12-05 03:11:55', '2018-12-05 03:11:55'),
-(6, 'mast', 'Raghib shahriyer', 'badda', '2018-12-05 07:22:25', '2018-12-05 07:22:25');
+(1, 'Hanif Enterprise', 'Mr. Sirajul Islam Hanif', 'Gabtoli bus stand', NULL, NULL),
+(2, 'Shyamoli NR Travels', 'Ganesh Chandra Ghosh', 'Mirpur-10, Bus stand', NULL, NULL),
+(8, 'Sonar Bangla', 'Raghib', 'Badda', '2018-12-06 05:05:30', '2018-12-06 05:05:30'),
+(9, 'Soudia', 'Raghib Shahriyer', 'Sherpur, Dhaka', '2018-12-06 05:08:47', '2018-12-06 05:08:47');
 
 -- --------------------------------------------------------
 
@@ -138,7 +137,10 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (33, '2018_12_02_121434_add_address_to_companies', 4),
 (34, '2018_12_02_122000_add_company_to_users', 5),
 (35, '2018_12_05_125926_add_company_image_to_users', 6),
-(36, '2018_12_05_130123_add_address_to_companies', 6);
+(36, '2018_12_05_130123_add_address_to_companies', 6),
+(37, '2018_12_06_152003_add_default_value_to_status_table', 7),
+(38, '2018_12_06_165410_add_column_available_to_buses_table', 8),
+(39, '2018_12_06_165502_remove_column_available_from_status_table', 8);
 
 -- --------------------------------------------------------
 
@@ -161,9 +163,8 @@ CREATE TABLE `password_resets` (
 CREATE TABLE `statuses` (
   `id` int(10) UNSIGNED NOT NULL,
   `bus_id` int(10) UNSIGNED NOT NULL,
-  `available` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `from` date DEFAULT NULL,
-  `till` date DEFAULT NULL,
+  `from` date DEFAULT '2000-11-04',
+  `till` date DEFAULT '2018-11-04',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -172,9 +173,9 @@ CREATE TABLE `statuses` (
 -- Dumping data for table `statuses`
 --
 
-INSERT INTO `statuses` (`id`, `bus_id`, `available`, `from`, `till`, `created_at`, `updated_at`) VALUES
-(1, 1, 'true', NULL, NULL, NULL, NULL),
-(2, 2, 'false', '2018-11-04', '2018-11-05', NULL, NULL);
+INSERT INTO `statuses` (`id`, `bus_id`, `from`, `till`, `created_at`, `updated_at`) VALUES
+(1, 1, '2018-11-04', '2018-11-05', NULL, NULL),
+(2, 2, '2018-11-04', '2018-11-05', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -200,12 +201,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `image`, `company`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(3, 'Orchid Sohag', '', '', 'Orchid@rana.com', NULL, '$2y$10$Whd8YrrCPnRIKJwikdNpQ.oyYhW1GaujG4SJahQZW2k9E29kN6DDK', 'eLRDDAxod021BWNwHTtdtnQZglU1W9fmkV6swDgKvXxPfLrLFApNAtxSvQ4x', '2018-12-03 13:43:11', '2018-12-03 13:43:11'),
-(4, 'Sohag', '', '', 'Shg@ranaa.com', NULL, '$2y$10$Mqfx51hOEDeUpsKm.NP3l.H9JIgts6/xJgVu1hQg0ynqWGXieEDK2', NULL, '2018-12-03 14:06:19', '2018-12-03 14:06:19'),
-(5, 'Sohag', '', '', 'Shgg@rana.com', NULL, '$2y$10$INpoFkAbIXgh30Yp.DdR6.f/8eXvRMYjbsMbjtY1frclKv0M7DuCO', '5fHr9Kq29MWB7IifumT7egqL8xB0JqwGQDRoXJjDlBVViEnJ1CfqG3J9cy1k', '2018-12-03 14:09:42', '2018-12-03 14:09:42'),
-(6, 'Sohag', '', '', 'Shhg@rana.com', NULL, '$2y$10$D9ITn1/p12zd8uSnkCdhrOr9TgP3SkOKOqQOyymX9CKJIZCwYn1se', 'HzKCX3AIyCYs4arv4K6t2b2efYkbgFLn6LZMpCgLnf9wSskHdEzg4HCaCkOp', '2018-12-03 14:15:48', '2018-12-03 14:15:48'),
-(7, 'Raghib', '', '', 'raghib@rana.com', NULL, '$2y$10$zdc.j4uO1braf7IkY2Uw8OncNGuiWj57sTXba/MVNLeh6J//MUgGC', 'sioR6k8DEGrI0IyVRmqRwMTwoWLZECDCP7SxuWHzi9BcK2v8dJohqLiRF6IS', '2018-12-05 03:11:55', '2018-12-05 03:11:55'),
-(8, 'Sohag', 'img/Users/1544016145image-1465348_960_720.jpg', 'mast', 'raghibb@rana.com', NULL, '$2y$10$4KjN0ELF0wK/n3QcRzplN.ZYTa2vfhQ1Kiqs2u29mxKg4ryjk/iuu', 'gPBtwyuQCMV5gWLyzg5sOksSAlj41rn60YVdfQzhx8hAf0eHcUvfDBybeiYJ', '2018-12-05 07:22:25', '2018-12-05 07:22:25');
+(8, 'Sohag', 'img/Users/1544016145image-1465348_960_720.jpg', 'mast', 'raghibb@rana.com', NULL, '$2y$10$4KjN0ELF0wK/n3QcRzplN.ZYTa2vfhQ1Kiqs2u29mxKg4ryjk/iuu', 'gPBtwyuQCMV5gWLyzg5sOksSAlj41rn60YVdfQzhx8hAf0eHcUvfDBybeiYJ', '2018-12-05 07:22:25', '2018-12-05 07:22:25'),
+(10, 'Sohag', 'img/Users/1544094330my-profile.jpg', 'Sonar Bangla', 'sohag@gmail.com', NULL, '$2y$10$DjoKhART3ycv8P.pJpDYVuFW8XA6dUjpUKjYb5UP4DV1V5JzTDimC', 'kVRAzCyopHpd3KUQxcXLjcQHGUPjY0VHAiCZ4w39hSV0ImVlMevNd5pz3FKM', '2018-12-06 05:05:30', '2018-12-06 05:05:30'),
+(11, 'Raghib', 'img/Users/1544094527admin2.jpg', 'Soudia', 'vcshahriyer@gmail.com', NULL, '$2y$10$nCafI7.AQp/4X7F7zgNcveZGt8gDePbxFgIbY.vR0V54agnebQDii', '41Ou4r9MT9MZ1uuiZws022YxSAAP1HPvMgmVR8SQDDpKgREtVgGOtzBbLBFo', '2018-12-06 05:08:47', '2019-04-19 03:11:57');
 
 --
 -- Indexes for dumped tables
@@ -266,37 +264,31 @@ ALTER TABLE `users`
 --
 ALTER TABLE `bookings`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
 --
 -- AUTO_INCREMENT for table `buses`
 --
 ALTER TABLE `buses`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `companies`
 --
 ALTER TABLE `companies`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
-
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 --
 -- AUTO_INCREMENT for table `statuses`
 --
 ALTER TABLE `statuses`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- Constraints for dumped tables
 --
@@ -318,7 +310,6 @@ ALTER TABLE `buses`
 --
 ALTER TABLE `statuses`
   ADD CONSTRAINT `statuses_bus_id_foreign` FOREIGN KEY (`bus_id`) REFERENCES `buses` (`id`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
